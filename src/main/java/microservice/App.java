b8f493ac5ca8077c48d28d37e3d58d9d6bfa11d0
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2018-06-12 17:01:58 +0530

    Basic Hierachy and template

---
+++ src/main/java/microservice/App.java
@@ -0,0 +1,13 @@
+package microservice;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class App {
+    public static void main(String [] args) throws Exception{
+
+    }
+}
---
+++ src/main/java/microservice/UserAPI.java
@@ -0,0 +1,7 @@
+package microservice;
+
+
+
+public class UserAPI {
+
+}
