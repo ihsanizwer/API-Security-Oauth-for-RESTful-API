3bc873145a69245c626cc6bcb5657a2b824a9aef
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2018-06-13 13:33:25 +0530

    Added basic RestfulAPI capabilities

--- src/main/java/microservice/App.java
+++ src/main/java/microservice/App.java
@@ -1,13 +1,11 @@
 package microservice;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.SpringApplication;
 
 public class App {
-    public static void main(String [] args) throws Exception{
 
+    public static void main(String [] args){
+        SpringApplication.run(UserAPI.class, args);
     }
+
 }
--- src/main/java/microservice/UserAPI.java
+++ src/main/java/microservice/UserAPI.java
@@ -1,7 +1,36 @@
 package microservice;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("api/user")
 public class UserAPI {
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    String list(){
+        return "Listing of User\n";
+    }
 
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    String add(String name){
+        return "Added User :"+name+"\n";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    String update(String name){
+        return "Updated user :"+name+"\n";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    @ResponseBody
+    String delete(String name){
+        return "Deleted User :"+name+"\n";
+    }
 }
