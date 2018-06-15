65d3135ae972834b1886ec244328130d6af1642c
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2018-06-15 16:40:04 +0530

    added basic Auth Server functionality

--- src/main/java/servlet/AuthorizationServer.java
+++ src/main/java/servlet/AuthorizationServer.java
@@ -1,10 +1,15 @@
 package servlet;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import data.AccessToken;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
 public class AuthorizationServer {
 
@@ -20,7 +25,18 @@ public class AuthorizationServer {
     public static class TokenIntrospectionEndpoint extends HttpServlet {
         @Override
         protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            req.getParameter("access_token");
+            String recToken = req.getParameter("access_token");
+            Map<String, Map<String, String>> tokenMap = AccessToken.getAccessToken();
+
+            if(tokenMap.get(recToken)!=null){
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("utf-8");
+                PrintWriter pw = resp.getWriter();
+
+            }else{
+                //Send error
+            }
+
         }
 
     }
